5e9c51bf32da0971a756a2b823dac642e9a2c3ec
Matthew Robitaille <matt@anchour.com>
2014-05-24 09:18:22 -0400

    Update Installer script to create .env file without DB_USER, HOST, etc. entries.

--- scripts/Bedrock/Installer.php
+++ scripts/Bedrock/Installer.php
@@ -16,8 +16,24 @@ class Installer {
     'NONCE_SALT'
   );
 
-  public static function addSalts(Event $event) {
-    $root = dirname(dirname(__DIR__));
+  /**
+   * Holds the root path.
+   * @var string
+   */
+  private static $root;
+
+  /**
+   * Holds the path to the .env file.
+   * @var [type]
+   */
+  private static $envFilePath;
+
+  public static function addSalts(Event $event)
+  {
+    // Set the paths, since PHP is stupid and was throwing a syntax error due to the dirname function.
+    static::$root = dirname(dirname(__DIR__));
+    static::$envFilePath = static::$root . '/.env';
+
     $composer = $event->getComposer();
     $io = $event->getIO();
 
@@ -32,24 +48,32 @@ class Installer {
     }
 
     $salts = array_map(function ($key) {
-      return sprintf("%s='%s'", $key, Installer::generate_salt());
+      return sprintf("%s='%s'", $key, Installer::generateSalt());
     }, self::$KEYS);
 
-    $env_file = "{$root}/.env";
-
-    if (copy("{$root}/.env.example", $env_file)) {
-      file_put_contents($env_file, implode($salts, "\n"), FILE_APPEND | LOCK_EX);
-    } else {
-      $io->write("<error>An error occured while copying your .env file</error>");
-      return 1;
+    if (! file_exists(static::$envFilePath)) {
+      static::createEnvFile();
     }
+
+    file_put_contents(static::$envFilePath, implode($salts, "\n"), FILE_APPEND | LOCK_EX);
+  }
+
+  /**
+   * Create/truncate the .env file to 0.
+   * @return resource
+   */
+  private static function createEnvFile()
+  {
+    $handle = fopen(static::$envFilePath, 'w');
+
+    fclose($handle);
   }
 
   /**
    * Slightly modified/simpler version of wp_generate_password
    * https://github.com/WordPress/WordPress/blob/cd8cedc40d768e9e1d5a5f5a08f1bd677c804cb9/wp-includes/pluggable.php#L1575
    */
-  public static function generate_salt($length = 64) {
+  public static function generateSalt($length = 64) {
     $chars  = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     $chars .= '!@#$%^&*()';
     $chars .= '-_ []{}<>~`+=,.;:/?|';
